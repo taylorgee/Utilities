250a6435a60b5d5d2f84031701f3f247c2b7a416
Taylor Gee <totaylorgee@gmail.com>
2021-04-18 17:06:12 -0700

    added enumerate and zip

--- Python/forLoop.py
+++ Python/forLoop.py
@@ -11,3 +11,17 @@ print('\n\n')
 # enumerate = printing the index 
 for number, fruit in enumerate(fruits):
     print(f"{number}: {fruit}")
+
+friends = ['Jazzy', 'Rachael', 'Hayden']
+
+for friend in friends:
+    print(f"\nmy friends are {friend}")
+    
+print()
+
+for number, friend in enumerate(friends):
+    print(f"{number}: {friend}")
+    
+# zip = two lists at the same time
+for fruit, name in zip(fruits, friends):
+    print(f'fruit:{fruit}\tname:{name}')
