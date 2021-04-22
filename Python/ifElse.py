647a13b1871f3bcf3d29ffdfe0ac9cd64df68912
Taylor Gee <totaylorgee@gmail.com>
2021-04-22 16:42:22 -0700

    new sample

---
+++ Python/ifElse.py
@@ -0,0 +1,20 @@
+friend = 'Jazzy'
+
+if friend == 'Jazzy':
+    print('yes 1')
+else: 
+    print('no 1')
+    
+counter = 1000
+if counter < 100: 
+    print('yes 2')
+else: 
+    print('no 2')
+    
+# and = both has to be true
+# or = just one needs to be true
+# if / else are conditions
+if friend == 'jazzy' or counter == 2000:
+    print('yes 3')
+else:
+    print('no 3')
